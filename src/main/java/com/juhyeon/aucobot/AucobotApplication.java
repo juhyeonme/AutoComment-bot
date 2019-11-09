4135bd0fe716f0a893bfda7e334f403da00b117a
juhyeon96 <kjh96905@gmail.com>
2019-11-09 20:17:32 +0900

    [#1] setting for Spring boot project

---
+++ src/main/java/com/juhyeon/aucobot/AucobotApplication.java
@@ -0,0 +1,13 @@
+package com.juhyeon.aucobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AucobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AucobotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juhyeon/aucobot/AucobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhyeon.aucobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AucobotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
