edb8cee3a1580a40cf082e45c764a9e9e7d43ea4
juhyeon96 <kjh96905@gmail.com>
2019-11-13 15:30:15 +0900

    [#6] change SDK from spring-social-github to egit-github

--- src/test/java/com/juhyeon/aucobot/AucobotApplicationTests.java
+++ src/test/java/com/juhyeon/aucobot/AucobotApplicationTests.java
@@ -1,28 +1,47 @@
 package com.juhyeon.aucobot;
 
-import com.juhyeon.aucobot.service.IssueService;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.RepositoryService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-class AucobotApplicationTests extends IssueService {
+@PropertySource("classpath:application.yml")
+class AucobotApplicationTests {
 
-    private static final String realCloneUrl = "https://github.com/juhyeon96/AutoComment-bot.git";
+    @Value("${spring.api.github.user}")
+    private String user;
+
+    @Value("${spring.api.github.password}")
+    private String password;
+
+    @Value("${spring.api.github.repo}")
+    private String repo;
+
     @Test
     void contextLoads() {
     }
 
     @Test
-    public void githubConnection() {
-        super.createGitHubTemplate();
-        String cloneUrl;
-        cloneUrl = this.githubTemplate.repoOperations().getRepo(user, repo).getCloneUrl();
+    public void connectEGitAndMyRepo() throws Exception {
+        String realCloneUrl = "https://github.com/juhyeon96/AutoComment-bot.git";
 
-        assertEquals(cloneUrl, realCloneUrl);
+        GitHubClient githubClient = new GitHubClient();
+        githubClient.setCredentials(user, password);
+        RepositoryService service = new RepositoryService();
+        Repository repository = service.getRepository(user, repo);
+
+        assertEquals(realCloneUrl, repository.getCloneUrl());
+        assertEquals(1, repository.getOpenIssues());
     }
 
 }
